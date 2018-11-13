f7148c23c1f35260cd07a038f466dbcbc8ca030b
sashikamw <sashika@wso2.com>
2018-11-13 20:51:04 +0530

    muliple users

--- product-scenarios/1-user-self-registration-to-an-application/1.1-user-registration-with-web-application-itself/1.1.1-provision-user-using-SCIM2/src/test/java/org/wso2/identity/scenarios/test/scim2/SCIMConstants.java
+++ product-scenarios/1-user-self-registration-to-an-application/1.1-user-registration-with-web-application-itself/1.1.1-provision-user-using-SCIM2/src/test/java/org/wso2/identity/scenarios/test/scim2/SCIMConstants.java
@@ -1,5 +1,8 @@
 package org.wso2.identity.scenarios.test.scim2;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
 public class SCIMConstants {
 
     public static final String SCIM2_USERS_ENDPOINT = "/scim2/Users";
@@ -15,6 +18,12 @@ public class SCIMConstants {
     public static final String PRIMARY_PARAM = "primary";
     public static final String NAME_ATTRIBUTE = "name";
     public static final String USER_NAME_ATTRIBUTE = "userName";
+    public static final String PATH_PARM = "/Users";
+    public static final String PATH_ATTRIBUTE = "path";
+    public static final String METHOD_PARM = "POST";
+    public static final String METHOD_ATTRIBUTE = "method";
+    public static final String FAIL_ON_ERROR_ATTRIBUTE = "failOnErrors";
+    public static final String BULK_ATTRIBUTE = "bulkId";
     public static final String PASSWORD_ATTRIBUTE = "password";
     public static final String ID_ATTRIBUTE = "id";
     public static final String GIVEN_NAME_CLAIM_VALUE = "user2";
@@ -23,4 +32,43 @@ public class SCIMConstants {
     public static final String PASSWORD = "scim2pwd";
     public static final String CONTENT_TYPE_APPLICATION_JSON = "application/json";
     public static final String ERROR_SCHEMA = "urn:ietf:params:scim:api:messages:2.0:Error";
+    public static final String BULK_SCHEMA = "urn:ietf:params:scim:api:messages:2.0:BulkRequest";
+    public static final String USER_SCHEMA = "urn:ietf:params:scim:schemas:core:2.0:User";
+
+
+    public void scimEnterpriseattributes(){
+    String a = "test";
+    }
+
+    public void scimBulkOperationattributes(){
+
+        JSONArray operations = new JSONArray();
+
+        JSONArray schemaUser = new JSONArray();
+        schemaUser.add("urn:ietf:params:scim:schemas:core:2.0:User");
+
+        JSONObject data1 = new JSONObject();
+        data1.put("schemas",schemaUser);
+        data1.put("userName","Kris90");
+        data1.put("password","password");
+
+        JSONObject bulkuser1 = new JSONObject();
+        bulkuser1.put("method","POST");
+        bulkuser1.put("path","/Users");
+        bulkuser1.put("bulkId","qwerty");
+        bulkuser1.put("data", data1);
+
+        bulkuser1.put("data", data1);
+
+        operations.add(bulkuser1);
+
+        System.out.print(operations);
+
+        return;
+
+/*        return operations;*/
+
+    }
+
+
 }
